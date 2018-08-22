6295ee87d4e91610f130b08ad79090c8cbec2e00
Dibyo Majumder <dibyomajumder23@gmail.com>
2018-08-22 11:07:54 +0530

    Smoke Test

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.2;
+
+contract Election {
+    //  Storing candidate
+    //  Reading candidate
+    string public candidate;
+    //  Constructor
+    constructor() public {
+        candidate = "candidate1";
+    }
+}
